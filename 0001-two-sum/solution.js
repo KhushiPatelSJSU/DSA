27125b7544611a6d42580df4184d0c6b71e7d3f9
Khushi <khushipatel20.cs@gmail.com>
2024-08-31 06:18:02 +0000

    [LeetCode Sync] Runtime - 157 ms (5.01%), Memory - 49.8 MB (100.00%)

---
+++ 0001-two-sum/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let j=0; j<nums.length; j++) {
+        for (let i=j+1; i<=nums.length; i++) {
+            if (nums[j]+nums[i]==target) return [j,i];
+            else continue;
+        }
+    }
+};
