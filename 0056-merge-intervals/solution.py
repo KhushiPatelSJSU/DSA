8ea95b52b77828cea9b61982fa36446c021c2dd2
Khushi <khushipatel20.cs@gmail.com>
2026-09-13 21:23:39 +0000

    [LeetCode] Runtime - 15 ms (7.27%), Memory - 22.6 MB (50.69%)

---
+++ 0056-merge-intervals/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+
+        intervals.sort()
+
+        for interval in intervals:
+            if not result:
+                result.append(interval)
+            elif result[-1][1] >= interval[0]:
+                result[-1][1] = max(result[-1][1], interval[1])
+            else:
+                result.append(interval)
+
+        return result
